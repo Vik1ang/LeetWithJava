30bfaf4fd0536e44ff610e3247923a209cf69be5
weiqiang <409655689@qq.com>
2021-01-08 14:30:25 +0800

    finish q617

---
+++ T1/src/tree/easy/q617/TreeNode.java
@@ -0,0 +1,22 @@
+package tree.easy.q617;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Channing
+ * @URL https://leetcode.com/problems/merge-two-binary-trees/
+ */
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
---
+++ T1/src/tree/easy/q617/q617.java
@@ -0,0 +1,11 @@
+package tree.easy.q617;
+
+/**
+ * @author Channing
+ * @URL https://leetcode.com/problems/merge-two-binary-trees/
+ */
+public class q617 {
+    public static void main(String[] args) {
+
+    }
+}
