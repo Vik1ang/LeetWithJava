40361baea7752f77a4a790a8b43e352be6a20f53
Vik1ang <409655689@qq.com>
2021-01-11 11:34:52 +0800

    produce a main function to help debug and make a template Solution.java

---
+++ T1/src/tree/easy/q617/Solution.java
@@ -0,0 +1,11 @@
+package tree.easy.q617;
+
+/**
+ * @author Vikiang
+ * @URL
+ */
+public class Solution {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        return null;
+    }
+}
--- T1/src/tree/easy/q617/q617.java
+++ T1/src/tree/easy/q617/q617.java
@@ -6,6 +6,32 @@ package tree.easy.q617;
  */
 public class q617 {
     public static void main(String[] args) {
+        TreeNode root1 = new TreeNode(1);
+        TreeNode root1LeftChild1 = new TreeNode(3);
+        root1.left = root1LeftChild1;
+        root1.right = new TreeNode(2);
+        root1LeftChild1.left = new TreeNode(5);
 
+        TreeNode root2 = new TreeNode(2);
+        TreeNode root2LeftChild1 = new TreeNode(1);
+        TreeNode root2RightChild1 = new TreeNode(3);
+        root2.left = root2LeftChild1;
+        root2.right = root2RightChild1;
+        root2LeftChild1.right = new TreeNode(4);
+        root2RightChild1.right = new TreeNode(7);
+
+        // make your own solution
+        TreeNode newTree = new Solution().mergeTrees(root1, root2);
+
+        preorder(newTree);
+
+    }
+
+    public static void preorder(TreeNode node) {
+        if (node != null) {
+            System.out.print(node.val + " ");
+            preorder(node.left);
+            preorder(node.right);
+        }
     }
 }
