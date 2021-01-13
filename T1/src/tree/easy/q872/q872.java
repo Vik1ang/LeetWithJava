266dbe11d7e032ce371f8cbb82e3ed5162e8463c
Vik1ang <409655689@qq.com>
2021-01-13 15:22:26 +0800

    finish q872

---
+++ T1/src/tree/easy/q872/q872.java
@@ -0,0 +1,11 @@
+package tree.easy.q872;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/leaf-similar-trees/
+ */
+public class q872 {
+    public static void main(String[] args) {
+
+    }
+}
