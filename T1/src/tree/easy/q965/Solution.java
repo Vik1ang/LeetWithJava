797a5e059c810dd501fe5cb6633471e4a7f8394a
Vik1ang <409655689@qq.com>
2021-01-13 10:38:21 +0800

    finish q965

---
+++ T1/src/tree/easy/q965/Solution.java
@@ -0,0 +1,11 @@
+package tree.easy.q965;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/univalued-binary-tree/
+ */
+public class Solution {
+    public boolean isUnivalTree(TreeNode root) {
+        return false;
+    }
+}
---
+++ T1/src/tree/easy/q965/q965.java
@@ -0,0 +1,11 @@
+package tree.easy.q965;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/univalued-binary-tree/
+ */
+public class q965 {
+    public static void main(String[] args) {
+
+    }
+}
