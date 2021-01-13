8cc8eab8425e2fd4ca14fdc664101cf9788118a1
Vik1ang <409655689@qq.com>
2021-01-13 11:57:54 +0800

    finish q226

---
+++ T1/src/tree/easy/q226/Solution.java
@@ -0,0 +1,11 @@
+package tree.easy.q226;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/invert-binary-tree/
+ */
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        return null;
+    }
+}
---
+++ T1/src/tree/easy/q226/q226.java
@@ -0,0 +1,9 @@
+package tree.easy.q226;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/invert-binary-tree/
+ */
+public class q226 {
+
+}
