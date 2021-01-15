5d9285c9d47e6122b2f6cafa0a379dad7098d85a
Vik1ang <409655689@qq.com>
2021-01-15 11:07:21 +0800

    finish q653

---
+++ T1/src/tree/easy/q653/Solution.java
@@ -0,0 +1,11 @@
+package tree.easy.q653;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/two-sum-iv-input-is-a-bst/
+ */
+public class Solution {
+    public boolean findTarget(TreeNode root, int k) {
+        return false;
+    }
+}
---
+++ T1/src/tree/easy/q653/q653.java
@@ -0,0 +1,11 @@
+package tree.easy.q653;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/two-sum-iv-input-is-a-bst/
+ */
+public class q653 {
+    public static void main(String[] args) {
+
+    }
+}
