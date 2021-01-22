0166aa0677e289e23b51ad7569907c746136c424
Vik1ang <409655689@qq.com>
2021-01-22 11:41:03 +0800

    finish q404

---
+++ T1/src/tree/easy/q404/Solution.java
@@ -0,0 +1,13 @@
+package tree.easy.q404;
+
+import tree.util.TreeNode;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/sum-of-left-leaves/
+ */
+public class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        return -1;
+    }
+}
