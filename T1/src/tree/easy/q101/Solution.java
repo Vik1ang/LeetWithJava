7c60be54254917f130382daadc3468e306512bbb
Vik1ang <409655689@qq.com>
2021-01-23 04:25:58 +0800

    finish q101

---
+++ T1/src/tree/easy/q101/Solution.java
@@ -0,0 +1,13 @@
+package tree.easy.q101;
+
+import tree.util.TreeNode;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/symmetric-tree/
+ */
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return false;
+    }
+}
---
+++ T1/src/tree/easy/q101/Solution1.java
@@ -0,0 +1,26 @@
+package tree.easy.q101;
+
+import tree.util.TreeNode;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/symmetric-tree/
+ */
+public class Solution1 {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return check(root, root);
+    }
+
+    private boolean check(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        return p.val == q.val && check(p.left, q.right) && check(p.right, q.left);
+    }
+}
