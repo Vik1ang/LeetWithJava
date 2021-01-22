a11cb2470a4e7c2a51c0977fbe22b21c65a7bc47
Vik1ang <409655689@qq.com>
2021-01-22 15:00:20 +0800

    add a function to get the treeNode in the tree

--- T1/src/tree/util/TreeHelper.java
+++ T1/src/tree/util/TreeHelper.java
@@ -44,6 +44,30 @@ public class TreeHelper {
 
     }
 
+    public TreeNode getTreeNode(TreeNode root, int val) {
+        if (root.val == val) {
+            return root;
+        }
+        boolean flag = false;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node.val == val) {
+                return node;
+            }
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+
+        return null;
+    }
+
 
     public void preorder(TreeNode node) {
         if (node == null) {
