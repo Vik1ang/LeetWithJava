64586d83b6a4c3e1a4d1cf3f50bffea1dcbf6817
Vik1ang <409655689@qq.com>
2021-01-23 01:38:08 +0800

    finish q543

---
+++ T1/src/tree/easy/q543/Solution2.java
@@ -0,0 +1,15 @@
+package tree.easy.q543;
+
+import tree.util.TreeNode;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/diameter-of-binary-tree/
+ */
+public class Solution2 {
+    // TODO: Try using iteration to solve this question
+    public int diameterOfBinaryTree(TreeNode root) {
+
+        return -1;
+    }
+}
---
+++ T1/src/tree/easy/q543/q543.java
@@ -0,0 +1,11 @@
+package tree.easy.q543;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/diameter-of-binary-tree/
+ */
+public class q543 {
+    public static void main(String[] args) {
+
+    }
+}
