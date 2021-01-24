d898d70470bbbc5db68b86e8ef042dc1eb5299ae
Vik1ang <409655689@qq.com>
2021-01-24 13:54:20 +0800

    finish q112

---
+++ T1/src/tree/easy/q112/Solution1.java
@@ -0,0 +1,20 @@
+package tree.easy.q112;
+
+import tree.util.TreeNode;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/path-sum/
+ */
+public class Solution1 {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return targetSum == root.val;
+        }
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+    }
+
+}
