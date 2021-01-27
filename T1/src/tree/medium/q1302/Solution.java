bafaf2f74b07143f21a0b3928fa9dedf910942b4
Vik1ang <409655689@qq.com>
2021-01-28 01:07:04 +0800

    finish q1302

---
+++ T1/src/tree/medium/q1302/Solution.java
@@ -0,0 +1,14 @@
+package tree.medium.q1302;
+
+import tree.util.TreeNode;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/deepest-leaves-sum/
+ */
+public class Solution {
+    public int deepestLeavesSum(TreeNode root) {
+        return -1;
+    }
+
+}
---
+++ T1/src/tree/medium/q1302/Solution1.java
@@ -0,0 +1,42 @@
+package tree.medium.q1302;
+
+import tree.util.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/deepest-leaves-sum/
+ */
+public class Solution1 {
+    public int deepestLeavesSum(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int max = -1;
+        int depth = 0;
+        int total = 0;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (depth > max) {
+                    max = depth;
+                    total = node.val;
+                } else if (depth == max) {
+                    total += node.val;
+                }
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            depth++;
+        }
+        return total;
+    }
+
+}
