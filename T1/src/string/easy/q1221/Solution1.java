83595a28d21b3ea349e6966982376ccbf54b7f0b
Vik1ang <409655689@qq.com>
2021-01-30 10:21:24 +0800

    finish q1221

---
+++ T1/src/string/easy/q1221/Solution1.java
@@ -0,0 +1,24 @@
+package string.easy.q1221;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/split-a-string-in-balanced-strings/
+ */
+public class Solution1 {
+    public int balancedStringSplit(String s) {
+        int numR = 0;
+        int numL = 0;
+        int res = 0;
+        for (char c : s.toCharArray()) {
+            if (c == 'R') {
+                numR++;
+            } else if (c == 'L') {
+                numL++;
+            }
+            if (numL == numR && numL != 0) {
+                res++;
+            }
+        }
+        return res;
+    }
+}
