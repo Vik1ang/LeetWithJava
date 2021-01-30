a04307b163f5e81b8c8368db8e6180c80126a69b
Vik1ang <409655689@qq.com>
2021-01-31 02:16:46 +0800

    finish q804

---
+++ T1/src/string/easy/q804/Solution.java
@@ -0,0 +1,11 @@
+package string.easy.q804;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/unique-morse-code-words/
+ */
+public class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        return -1;
+    }
+}
