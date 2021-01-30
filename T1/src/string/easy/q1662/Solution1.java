5d3c9562d725b6b8f5b8a516b066247012aef375
Vik1ang <409655689@qq.com>
2021-01-30 12:48:57 +0800

    finish q1662

---
+++ T1/src/string/easy/q1662/Solution1.java
@@ -0,0 +1,20 @@
+package string.easy.q1662;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode-cn.com/problems/check-if-two-string-arrays-are-equivalent/
+ */
+public class Solution1 {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        String w1 = "";
+        String w2 = "";
+        for (String s : word1) {
+            w1 += s;
+        }
+        for (String s : word2) {
+            w2 += s;
+        }
+        return w1.equals(w2);
+
+    }
+}
---
+++ T1/src/string/easy/q1662/q1662.java
@@ -0,0 +1,11 @@
+package string.easy.q1662;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode-cn.com/problems/check-if-two-string-arrays-are-equivalent/
+ */
+public class q1662 {
+    public static void main(String[] args) {
+
+    }
+}
