eb90e7a57bad8ef80e9b70ba0313e131f1ab4f4a
Vik1ang <409655689@qq.com>
2021-01-31 18:15:26 +0800

    finish q1436

---
+++ T1/src/string/easy/q1436/Solution.java
@@ -0,0 +1,13 @@
+package string.easy.q1436;
+
+import java.util.List;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/destination-city/
+ */
+public class Solution {
+    public String destCity(List<List<String>> paths) {
+        return null;
+    }
+}
