d6c0de7b93fa6e1394d5a06825cc3a371c35ab6b
Vik1ang <409655689@qq.com>
2021-02-01 11:14:14 +0800

    finish q657

---
+++ T1/src/string/easy/q657/Solution.java
@@ -0,0 +1,11 @@
+package string.easy.q657;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/robot-return-to-origin/
+ */
+public class Solution {
+    public boolean judgeCircle(String moves) {
+        return false;
+    }
+}
