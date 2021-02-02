c14b162b62251c3aa6dac4b073cc24d423a33a37
Vik1ang <409655689@qq.com>
2021-02-02 13:16:10 +0800

    finish q929

---
+++ T1/src/string/easy/q929/Solution1.java
@@ -0,0 +1,31 @@
+package string.easy.q929;
+
+import java.util.HashSet;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/unique-email-addresses/
+ */
+public class Solution1 {
+    public int numUniqueEmails(String[] emails) {
+        HashSet<String> hashSet = new HashSet<>();
+        for (String email : emails) {
+            int mid = email.indexOf("@");
+            String first = email.substring(0, mid);
+            String last = email.substring(mid);
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < first.length(); i++) {
+                if (first.charAt(i) == '+') {
+                    break;
+                } else if (first.charAt(i) == '.') {
+                    continue;
+                } else {
+                    builder.append(first.charAt(i));
+                }
+            }
+            String newEmail = builder.toString() + last;
+            hashSet.add(newEmail);
+        }
+        return hashSet.size();
+    }
+}
---
+++ T1/src/string/easy/q929/q929.java
@@ -0,0 +1,15 @@
+package string.easy.q929;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/unique-email-addresses/
+ */
+public class q929 {
+    public static void main(String[] args) {
+        new Solution1().numUniqueEmails(new String[]{
+                "test.email+alex@leetcode.com",
+                "test.e.mail+bob.cathy@leetcode.com",
+                "testemail+david@lee.tcode.com"
+        });
+    }
+}
