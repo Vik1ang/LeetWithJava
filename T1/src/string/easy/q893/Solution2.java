50854ec35dd494c4e53500696bfdf0c139440a9c
Vik1ang <409655689@qq.com>
2021-02-02 11:02:11 +0800

    finish q893

---
+++ T1/src/string/easy/q893/Solution2.java
@@ -0,0 +1,11 @@
+package string.easy.q893;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/groups-of-special-equivalent-strings/
+ */
+public class Solution2 {
+    public int numSpecialEquivGroups(String[] A) {
+        return -1;
+    }
+}
