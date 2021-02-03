f316db7207ed301cb12262bf7f8e602b83840351
Vik1ang <409655689@qq.com>
2021-02-03 15:39:56 +0800

    finish q1446

---
+++ T1/src/string/easy/q1446/Solution2.java
@@ -0,0 +1,11 @@
+package string.easy.q1446;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/consecutive-characters/
+ */
+public class Solution2 {
+    public int maxPower(String s) {
+        return -1;
+    }
+}
