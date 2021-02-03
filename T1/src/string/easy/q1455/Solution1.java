b98cc0ee899be02ed1df89b0a16496f5b2d745d9
Vik1ang <409655689@qq.com>
2021-02-03 11:14:31 +0800

    finish q1455

---
+++ T1/src/string/easy/q1455/Solution1.java
@@ -0,0 +1,11 @@
+package string.easy.q1455;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/
+ */
+public class Solution1 {
+    public int isPrefixOfWord(String sentence, String searchWord) {
+        return -1;
+    }
+}
---
+++ T1/src/string/easy/q1455/Solution2.java
@@ -0,0 +1,17 @@
+package string.easy.q1455;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/
+ */
+public class Solution2 {
+    public int isPrefixOfWord(String sentence, String searchWord) {
+        String[] words = sentence.split(" ");
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].startsWith(searchWord)) {
+                return i + 1;
+            }
+        }
+        return -1;
+    }
+}
