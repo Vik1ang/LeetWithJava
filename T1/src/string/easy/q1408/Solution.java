3e9c44c7fd09e2791d3b190b94964ec87810ef55
Vik1ang <409655689@qq.com>
2021-02-03 12:03:59 +0800

    finish q1408

---
+++ T1/src/string/easy/q1408/Solution.java
@@ -0,0 +1,13 @@
+package string.easy.q1408;
+
+import java.util.List;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/string-matching-in-an-array/
+ */
+public class Solution {
+    public List<String> stringMatching(String[] words) {
+        return null;
+    }
+}
