15f82496b37d9cff626d0ebe691d18f1f03e8f4d
Vik1ang <409655689@qq.com>
2021-02-04 23:33:09 +0800

    finish q521

---
+++ T1/src/string/easy/q521/Solution2.java
@@ -0,0 +1,15 @@
+package string.easy.q521;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/longest-uncommon-subsequence-i/
+ */
+public class Solution2 {
+    public int findLUSlength(String a, String b) {
+        if (a.contains(b)) {
+            return -1;
+        } else {
+            return Math.max(a.length(), b.length());
+        }
+    }
+}
