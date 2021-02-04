ee44967bb38ffde3a23d7f1b4b58fb34d4bf764c
Vik1ang <409655689@qq.com>
2021-02-04 22:28:32 +0800

    finish q1624

---
+++ T1/src/string/easy/q1624/q1624.java
@@ -0,0 +1,11 @@
+package string.easy.q1624;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/largest-substring-between-two-equal-characters/
+ */
+public class q1624 {
+    public static void main(String[] args) {
+        System.out.println(new Solution1().maxLengthBetweenEqualCharacters("aa"));
+    }
+}
