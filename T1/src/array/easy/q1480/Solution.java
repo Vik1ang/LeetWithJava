b5a65bc940e7d6826f817d549de58c657aa22c11
Vik1ang <409655689@qq.com>
2021-02-05 12:03:46 +0800

    finish q1480

---
+++ T1/src/array/easy/q1480/Solution.java
@@ -0,0 +1,11 @@
+package array.easy.q1480;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/running-sum-of-1d-array/
+ */
+public class Solution {
+    public int[] runningSum(int[] nums) {
+        return null;
+    }
+}
---
+++ T1/src/array/easy/q1480/q1480.java
@@ -0,0 +1,11 @@
+package array.easy.q1480;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/running-sum-of-1d-array/
+ */
+public class q1480 {
+    public static void main(String[] args) {
+        System.out.println(new Solution2().runningSum(new int[]{1, 2, 3, 4}));
+    }
+}
