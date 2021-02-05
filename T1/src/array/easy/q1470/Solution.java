d074e0c0698d2c0043178acf05d691ff8e92cbe6
Vik1ang <409655689@qq.com>
2021-02-05 21:40:30 +0800

    finish q1470

---
+++ T1/src/array/easy/q1470/Solution.java
@@ -0,0 +1,11 @@
+package array.easy.q1470;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/shuffle-the-array/
+ */
+public class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        return null;
+    }
+}
---
+++ T1/src/array/easy/q1470/q1470.java
@@ -0,0 +1,11 @@
+package array.easy.q1470;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/shuffle-the-array/
+ */
+public class q1470 {
+    public static void main(String[] args) {
+
+    }
+}
