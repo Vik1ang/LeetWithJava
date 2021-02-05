6360779b8a04729fbd90685753ebdd1da79d081e
Vik1ang <409655689@qq.com>
2021-02-05 21:47:59 +0800

    finish q1672

---
+++ T1/src/array/easy/q1672/q1672.java
@@ -0,0 +1,11 @@
+package array.easy.q1672;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/richest-customer-wealth/
+ */
+public class q1672 {
+    public static void main(String[] args) {
+
+    }
+}
