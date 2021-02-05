d55bd14c9f5fea51e9d38943460a0d088f64a7b3
Vik1ang <409655689@qq.com>
2021-02-05 11:44:11 +0800

    finish q922

---
+++ T1/src/array/easy/q922/Solution3.java
@@ -0,0 +1,27 @@
+package array.easy.q922;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/sort-array-by-parity-ii/
+ */
+public class Solution3 {
+    public int[] sortArrayByParityII(int[] A) {
+        int n = A.length;
+        int j = 1;
+        for (int i = 0; i < n; i += 2) {
+            if (A[i] % 2 == 1) {
+                while (A[j] % 2 == 1) {
+                    j += 2;
+                }
+                swap(A, i, j);
+            }
+        }
+        return A;
+    }
+
+    private void swap(int[] A, int i, int j) {
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
+}
