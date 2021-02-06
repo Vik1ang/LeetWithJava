b2203623baedb5b0776505ad457e959201185fef
Vik1ang <409655689@qq.com>
2021-02-06 13:41:26 +0800

    finish q1389

---
+++ T1/src/array/easy/q1389/Solution.java
@@ -0,0 +1,11 @@
+package array.easy.q1389;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/create-target-array-in-the-given-order/
+ */
+public class Solution {
+    public int[] createTargetArray(int[] nums, int[] index) {
+        return null;
+    }
+}
---
+++ T1/src/array/easy/q1389/q1389.java
@@ -0,0 +1,11 @@
+package array.easy.q1389;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/create-target-array-in-the-given-order/
+ */
+public class q1389 {
+    public static void main(String[] args) {
+
+    }
+}
