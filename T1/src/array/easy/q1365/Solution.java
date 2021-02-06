fdbd726a86a2c514047c3b2f27019c1dec132155
Vik1ang <409655689@qq.com>
2021-02-06 10:32:14 +0800

    finish q1365

---
+++ T1/src/array/easy/q1365/Solution.java
@@ -0,0 +1,11 @@
+package array.easy.q1365;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+ */
+public class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        return null;
+    }
+}
