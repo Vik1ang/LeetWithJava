04e5b9f574fcc4d2e5d7e99904bb8deae994d9ba
Vik1ang <409655689@qq.com>
2021-02-07 02:15:11 +0800

    finish q1588

---
+++ T1/src/array/easy/q1588/q1588.java
@@ -0,0 +1,12 @@
+package array.easy.q1588;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/sum-of-all-odd-length-subarrays/
+ */
+public class q1588 {
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 2, 5, 3};
+        System.out.println(new Solution1().sumOddLengthSubarrays(arr));
+    }
+}
