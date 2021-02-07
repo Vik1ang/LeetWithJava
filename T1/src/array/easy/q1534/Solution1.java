ad183e99fd1f417ac162483b701b43319273d3de
Vik1ang <409655689@qq.com>
2021-02-07 11:26:58 +0800

    finish q1534

---
+++ T1/src/array/easy/q1534/Solution1.java
@@ -0,0 +1,22 @@
+package array.easy.q1534;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/count-good-triplets/
+ */
+public class Solution1 {
+    public int countGoodTriplets(int[] arr, int a, int b, int c) {
+        int count = 0;
+        int len = arr.length;
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                for (int k = j + 1; k < len; k++) {
+                    if (Math.abs(arr[i] - arr[j]) <= a && Math.abs(arr[j] - arr[k]) <= b && Math.abs(arr[k] - arr[i]) <= c) {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
