324b05aed180a70df605ef478de78882f9ee98aa
Vik1ang <409655689@qq.com>
2021-02-07 23:51:44 +0800

    finish q1295

---
+++ T1/src/array/easy/q1295/Solution.java
@@ -0,0 +1,11 @@
+package array.easy.q1295;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+ */
+public class Solution {
+    public int findNumbers(int[] nums) {
+        return -1;
+    }
+}
