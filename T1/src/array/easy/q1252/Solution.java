0c28600c123c0bd0566171333f63e680bd151385
Vik1ang <409655689@qq.com>
2021-02-08 00:41:50 +0800

    finish q1252

---
+++ T1/src/array/easy/q1252/Solution.java
@@ -0,0 +1,11 @@
+package array.easy.q1252;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/cells-with-odd-values-in-a-matrix/
+ */
+public class Solution {
+    public int oddCells(int n, int m, int[][] indices) {
+        return -1;
+    }
+}
