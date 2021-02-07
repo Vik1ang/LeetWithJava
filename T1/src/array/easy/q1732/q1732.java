e5318d722821dd209a80ecfa83cdc858c187571c
Vik1ang <409655689@qq.com>
2021-02-07 11:07:27 +0800

    finish q1732

---
+++ T1/src/array/easy/q1732/q1732.java
@@ -0,0 +1,11 @@
+package array.easy.q1732;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/find-the-highest-altitude/
+ */
+public class q1732 {
+    public static void main(String[] args) {
+
+    }
+}
