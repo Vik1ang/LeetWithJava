7d2aba80f7368241c8044a258fb2213590506c2d
Vik1ang <409655689@qq.com>
2021-02-07 16:28:10 +0800

    finish q1266

---
+++ T1/src/array/easy/q1266/Solution2.java
@@ -0,0 +1,12 @@
+package array.easy.q1266;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/minimum-time-visiting-all-points/
+ */
+public class Solution2 {
+    public int minTimeToVisitAllPoints(int[][] points) {
+
+        return -1;
+    }
+}
---
+++ T1/src/array/easy/q1266/q1266.java
@@ -0,0 +1,11 @@
+package array.easy.q1266;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/minimum-time-visiting-all-points/
+ */
+public class q1266 {
+    public static void main(String[] args) {
+
+    }
+}
