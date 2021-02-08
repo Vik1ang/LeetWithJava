592cf82ca506af7ddf71591e794c623cb0428d67
Vik1ang <409655689@qq.com>
2021-02-08 11:12:50 +0800

    finish q1748

---
+++ T1/src/array/easy/q1748/Solution.java
@@ -0,0 +1,11 @@
+package array.easy.q1748;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/sum-of-unique-elements/
+ */
+public class Solution {
+    public int sumOfUnique(int[] nums) {
+        return -1;
+    }
+}
