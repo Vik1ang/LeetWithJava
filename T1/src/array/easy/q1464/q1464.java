b7f2bad80e9937db89668a21d564d5660d803858
Vik1ang <409655689@qq.com>
2021-02-08 13:28:47 +0800

    finish q1464

---
+++ T1/src/array/easy/q1464/q1464.java
@@ -0,0 +1,11 @@
+package array.easy.q1464;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/maximum-product-of-two-elements-in-an-array/
+ */
+public class q1464 {
+    public static void main(String[] args) {
+
+    }
+}
