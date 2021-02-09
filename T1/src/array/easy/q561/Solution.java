76627101e23410a3637c5619eaf6a7721bdf58aa
Vik1ang <409655689@qq.com>
2021-02-09 14:30:23 +0800

    finish q561

---
+++ T1/src/array/easy/q561/Solution.java
@@ -0,0 +1,11 @@
+package array.easy.q561;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/array-partition-i/
+ */
+public class Solution {
+    public int arrayPairSum(int[] nums) {
+        return -1;
+    }
+}
