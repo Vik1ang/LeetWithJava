a7bab64fe36c0d4cce79acc179a37004cd216a95
Vik1ang <409655689@qq.com>
2021-02-09 14:35:43 +0800

    finish q977

---
+++ T1/src/array/easy/q977/Solution2.java
@@ -0,0 +1,13 @@
+package array.easy.q977;
+
+import java.util.Arrays;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/squares-of-a-sorted-array/
+ */
+public class Solution2 {
+    public int[] sortedSquares(int[] nums) {
+        return Arrays.stream(nums).map(e -> e * e).sorted().toArray();
+    }
+}
