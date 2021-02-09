6e04188354a3e364d155b34a07bd03749eeabfda
Vik1ang <409655689@qq.com>
2021-02-09 11:32:26 +0800

    finish q1351

---
+++ T1/src/array/easy/q1351/Solution1.java
@@ -0,0 +1,29 @@
+package array.easy.q1351;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/
+ */
+public class Solution1 {
+    public int countNegatives(int[][] grid) {
+        int num = 0;
+        for (int[] ints : grid) {
+            int l = 0;
+            int r = ints.length - 1;
+            int pos = -1;
+            while (l <= r) {
+                int mid = l + ((r - l) >> 1);
+                if (ints[mid] < 0) {
+                    pos = mid;
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            }
+            if (~pos < 0) {
+                num += ints.length - pos;
+            }
+        }
+        return num;
+    }
+}
---
+++ T1/src/array/easy/q1351/q1351.java
@@ -0,0 +1,11 @@
+package array.easy.q1351;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/
+ */
+public class q1351 {
+    public static void main(String[] args) {
+
+    }
+}
