cf158c39c4cfcce6c3c2f043eba01c25724b6853
Vik1ang <409655689@qq.com>
2021-02-09 13:05:13 +0800

    finish q905

---
+++ T1/src/array/easy/q905/Solution.java
@@ -0,0 +1,11 @@
+package array.easy.q905;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/sort-array-by-parity/
+ */
+public class Solution {
+    public int[] sortArrayByParity(int[] A) {
+        return null;
+    }
+}
