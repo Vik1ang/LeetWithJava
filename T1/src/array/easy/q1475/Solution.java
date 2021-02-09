516e1657ce804938b02640c32842d0faea693ac6
Vik1ang <409655689@qq.com>
2021-02-09 10:01:27 +0800

    finish q1475

---
+++ T1/src/array/easy/q1475/Solution.java
@@ -0,0 +1,11 @@
+package array.easy.q1475;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode-cn.com/problems/final-prices-with-a-special-discount-in-a-shop/
+ */
+public class Solution {
+    public int[] finalPrices(int[] prices) {
+        return null;
+    }
+}
---
+++ T1/src/array/easy/q1475/q1475.java
@@ -0,0 +1,11 @@
+package array.easy.q1475;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode-cn.com/problems/final-prices-with-a-special-discount-in-a-shop/
+ */
+public class q1475 {
+    public static void main(String[] args) {
+        new Solution2().finalPrices(new int[]{8, 4, 6, 2, 3});
+    }
+}
