78ae03e09d77e05c94bf09e089ebfa6dab26d99e
Vik1ang <409655689@qq.com>
2021-02-09 13:45:19 +0800

    finish q1742

---
+++ T1/src/array/easy/q1742/q1742.java
@@ -0,0 +1,11 @@
+package array.easy.q1742;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/maximum-number-of-balls-in-a-box/
+ */
+public class q1742 {
+    public static void main(String[] args) {
+        new Solution1().countBalls(5, 15);
+    }
+}
