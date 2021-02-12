59386577c345164369c298fc7a6ca39e77d9efe2
Vik1ang <409655689@qq.com>
2021-02-13 01:58:50 +0800

    finish q1122

---
+++ T1/src/array/easy/q1122/Solution.java
@@ -0,0 +1,11 @@
+package array.easy.q1122;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/relative-sort-array/
+ */
+public class Solution {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        return null;
+    }
+}
