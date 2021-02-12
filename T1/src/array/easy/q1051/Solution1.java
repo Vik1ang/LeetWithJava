68082c75e6e7ca6552a32fe6f583e018117e20b0
Vik1ang <409655689@qq.com>
2021-02-12 23:47:17 +0800

    finish q1051

---
+++ T1/src/array/easy/q1051/Solution1.java
@@ -0,0 +1,27 @@
+package array.easy.q1051;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/height-checker/
+ */
+public class Solution1 {
+    public int heightChecker(int[] heights) {
+        List<Integer> copy = Arrays.stream(heights).boxed().collect(Collectors.toList());
+        copy.sort(Comparator.naturalOrder());
+        int count = 0;
+
+        for (int i = 0; i < heights.length; i++) {
+            if (copy.get(i) == heights[i]) {
+                continue;
+            } else {
+                count++;
+            }
+        }
+        return count;
+    }
+}
