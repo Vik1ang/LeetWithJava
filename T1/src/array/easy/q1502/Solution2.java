634870826a2e507d0723e0cb7b810d0dceac9999
Vik1ang <409655689@qq.com>
2021-02-17 23:06:27 +0800

    finish q1502

---
+++ T1/src/array/easy/q1502/Solution2.java
@@ -0,0 +1,19 @@
+package array.easy.q1502;
+
+import java.util.Arrays;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/can-make-arithmetic-progression-from-sequence/
+ */
+public class Solution2 {
+    public boolean canMakeArithmeticProgression(int[] arr) {
+        Arrays.sort(arr);
+        for (int i = 1; i < arr.length - 1; i++) {
+            if (arr[i] * 2 != arr[i - 1] + arr[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ T1/src/array/easy/q1502/q1502.java
@@ -0,0 +1,11 @@
+package array.easy.q1502;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/can-make-arithmetic-progression-from-sequence/
+ */
+public class q1502 {
+    public static void main(String[] args) {
+
+    }
+}
