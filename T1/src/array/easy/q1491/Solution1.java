30e6a28fd30f6040f34c8d2f218acff91047db6a
Vik1ang <409655689@qq.com>
2021-02-19 10:20:03 +0800

    finish q1491

---
+++ T1/src/array/easy/q1491/Solution1.java
@@ -0,0 +1,18 @@
+package array.easy.q1491;
+
+import java.util.Arrays;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/average-salary-excluding-the-minimum-and-maximum-salary/`
+ */
+public class Solution1 {
+    public double average(int[] salary) {
+        Arrays.sort(salary);
+        double count = 0;
+        for (int i = 1; i < salary.length - 1; i++) {
+            count += salary[i];
+        }
+        return (double)(count/(salary.length - 2));
+    }
+}
