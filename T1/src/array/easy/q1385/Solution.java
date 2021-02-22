c9c40740fced98dcf67afb9d0afc4f10d489bbef
Vik1ang <409655689@qq.com>
2021-02-22 10:52:07 +0800

    finish q1385

---
+++ T1/src/array/easy/q1385/Solution.java
@@ -0,0 +1,11 @@
+package array.easy.q1385;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/find-the-distance-value-between-two-arrays/
+ */
+public class Solution {
+    public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
+        return -1;
+    }
+}
