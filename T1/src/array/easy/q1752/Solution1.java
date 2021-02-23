366743cdde57b8f3403f0eb2886ad26dcf67424b
Vik1ang <409655689@qq.com>
2021-02-23 13:47:20 +0800

    finish q1752

---
+++ T1/src/array/easy/q1752/Solution1.java
@@ -0,0 +1,25 @@
+package array.easy.q1752;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/
+ */
+public class Solution1 {
+    public boolean check(int[] nums) {
+        int n = nums.length;
+        boolean isCount = nums[0] >= nums[n-1];
+
+        for(int i=1;i<n;i++){
+
+            if(nums[i]<nums[i-1]){
+                if(isCount){
+                    isCount = false;
+                }else{
+                    return false;
+                }
+            }
+
+        }
+        return true;
+    }
+}
