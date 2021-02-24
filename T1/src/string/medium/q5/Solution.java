75b9ed676735132d50a33cced3659a4f3e873e6f
Vik1ang <409655689@qq.com>
2021-02-24 23:31:00 +0800

    finish q5

---
+++ T1/src/string/medium/q5/Solution.java
@@ -0,0 +1,11 @@
+package string.medium.q5;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/longest-palindromic-substring/
+ */
+public class Solution {
+    public String longestPalindrome(String s) {
+        return null;
+    }
+}
