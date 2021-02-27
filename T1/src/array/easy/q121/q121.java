a250a7b25389b8aea8c729f38bc9203895879a48
Vik1ang <409655689@qq.com>
2021-02-27 21:53:30 +0800

    finish q121

---
+++ T1/src/array/easy/q121/q121.java
@@ -0,0 +1,11 @@
+package array.easy.q121;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+ */
+public class q121 {
+    public static void main(String[] args) {
+
+    }
+}
