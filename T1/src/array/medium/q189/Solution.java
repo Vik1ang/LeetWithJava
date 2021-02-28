3fe63d962d2c7a4629d4ff66b90d084b654a5238
Vik1ang <409655689@qq.com>
2021-02-28 10:46:17 +0800

    finish q189

---
+++ T1/src/array/medium/q189/Solution.java
@@ -0,0 +1,11 @@
+package array.medium.q189;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/rotate-array/
+ */
+public class Solution {
+    public void rotate(int[] nums, int k) {
+
+    }
+}
---
+++ T1/src/array/medium/q189/q189.java
@@ -0,0 +1,11 @@
+package array.medium.q189;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/rotate-array/
+ */
+public class q189 {
+    public static void main(String[] args) {
+
+    }
+}
