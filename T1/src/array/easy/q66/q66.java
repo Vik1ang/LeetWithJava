25c4693d141dacb89314e5bcd99a84e4d06503c1
Vik1ang <409655689@qq.com>
2021-02-28 12:26:52 +0800

    finish q66

---
+++ T1/src/array/easy/q66/q66.java
@@ -0,0 +1,11 @@
+package array.easy.q66;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/plus-one/
+ */
+public class q66 {
+    public static void main(String[] args) {
+
+    }
+}
