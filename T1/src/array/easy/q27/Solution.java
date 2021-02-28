17b926fd5b8e117fc6bca17cf2dc95255ff5d2eb
Vik1ang <409655689@qq.com>
2021-02-28 11:03:50 +0800

    finish q27

---
+++ T1/src/array/easy/q27/Solution.java
@@ -0,0 +1,11 @@
+package array.easy.q27;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/remove-element/
+ */
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+        return -1;
+    }
+}
---
+++ T1/src/array/easy/q27/q27.java
@@ -0,0 +1,8 @@
+package array.easy.q27;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/remove-element/
+ */
+public class q27 {
+}
