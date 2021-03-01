1dce47b2928ec630cefb0be8ad6199e68a165b5c
Vik1ang <409655689@qq.com>
2021-03-01 11:17:35 +0800

    add ListNode helper

---
+++ T1/src/LinkedList/util/ListNode.java
@@ -0,0 +1,24 @@
+package LinkedList.util;
+
+/**
+ * @author Vikiang
+ * @URL
+ */
+
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    public ListNode() {
+    }
+}
