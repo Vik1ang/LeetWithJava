6c08e8d4c7c36872e2d84775d0a30114b449d16e
Vik1ang <409655689@qq.com>
2021-03-04 00:34:36 +0800

    finish q2

--- T1/src/LinkedList/medium/q2/Solution1.java
+++ T1/src/LinkedList/medium/q2/Solution1.java
@@ -7,24 +7,35 @@ import LinkedList.util.ListNode;
  * @URL https://leetcode.com/problems/add-two-numbers/
  */
 public class Solution1 {
+    // Does not work if the sum is greater than int range
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         StringBuilder s1 = new StringBuilder();
         StringBuilder s2 = new StringBuilder();
-        while (l1 != null) {
-            s1.append(String.valueOf(l1.val));
-            l1 = l1.next;
+        ListNode head = new ListNode(0);
+        while (l1 != null || l2 != null) {
+            if (l1 != null) {
+                s1.append(l1.val);
+            }
+            if (l2 != null) {
+                s2.append(l2.val);
+            }
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
         }
-        while (l2 != null) {
-            s2.append(String.valueOf(l2.val));
-        }
-        ListNode listNode = new ListNode(0);
-        ListNode temp = listNode;
+        s1.reverse();
+        s2.reverse();
         int sum = Integer.parseInt(s1.toString()) + Integer.parseInt(s2.toString());
-        String str = String.valueOf(sum);
-        for (int i = 0; i < str.length(); i++) {
-            temp.next = new ListNode(Integer.parseInt(String.valueOf(str.charAt(i))));
-            temp = temp.next;
+        StringBuilder sb = new StringBuilder(String.valueOf(sum));
+        sb.reverse();
+        ListNode curr = head;
+        for (int i = 0; i < sb.length(); i++) {
+            curr.next = new ListNode((int) sb.charAt(i) - (int) ('0'));
+            curr = curr.next;
         }
-        return listNode.next;
+        return head.next;
     }
 }
--- T1/src/LinkedList/medium/q2/Solution2.java
+++ T1/src/LinkedList/medium/q2/Solution2.java
@@ -8,14 +8,16 @@ import LinkedList.util.ListNode;
  */
 public class Solution2 {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode head = null, tail = null;
+        ListNode head = null;
+        ListNode tail = null;
         int carry = 0;
         while (l1 != null || l2 != null) {
             int n1 = l1 != null ? l1.val : 0;
             int n2 = l2 != null ? l2.val : 0;
             int sum = n1 + n2 + carry;
             if (head == null) {
-                head = tail = new ListNode(sum % 10);
+                tail = new ListNode(sum % 10);
+                head = tail;
             } else {
                 tail.next = new ListNode(sum % 10);
                 tail = tail.next;
