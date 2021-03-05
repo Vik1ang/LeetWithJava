70a237554d9164effda46e9b17c2ead620a14cc7
Vik1ang <409655689@qq.com>
2021-03-05 13:54:59 +0800

    finish q1143

---
+++ T1/src/DynamicProgramming/medium/q1143/Solution.java
@@ -0,0 +1,11 @@
+package DynamicProgramming.medium.q1143;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/longest-common-subsequence/
+ */
+public class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        return -1;
+    }
+}
---
+++ T1/src/DynamicProgramming/medium/q1143/q1143.java
@@ -0,0 +1,8 @@
+package DynamicProgramming.medium.q1143;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/longest-common-subsequence/
+ */
+public class q1143 {
+}
