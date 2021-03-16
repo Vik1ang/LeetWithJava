25e7245d95b31196dd8508fe9a9616a7eaaaf00a
Vik1ang <409655689@qq.com>
2021-03-16 14:46:08 +0800

    finish q705

---
+++ T1/src/HashTable/easy/q705/MyHashSet.java
@@ -0,0 +1,63 @@
+package HashTable.easy.q705;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/design-hashset/
+ */
+public class MyHashSet {
+    private static final int BASE = 769;
+    private LinkedList[] data;
+
+    public MyHashSet() {
+        data = new LinkedList[BASE];
+        for (int i = 0; i < BASE; i++) {
+            data[i] = new LinkedList<Integer>();
+        }
+    }
+
+    public void add(int key) {
+        int h = hash(key);
+        Iterator<Integer> iterator = data[h].iterator();
+        while (iterator.hasNext()) {
+            Integer element = iterator.next();
+            if (element == key) {
+                return;
+            }
+        }
+        data[h].offerLast(key);
+    }
+
+    private static int hash(int key) {
+        return key % BASE;
+    }
+
+    public void remove(int key) {
+        int h = hash(key);
+        Iterator<Integer> iterator = data[h].iterator();
+        while (iterator.hasNext()) {
+            Integer element = iterator.next();
+            if (element == key) {
+                data[h].remove(element);
+                return;
+            }
+        }
+    }
+
+    /**
+     * Returns true if this set contains the specified element
+     */
+    public boolean contains(int key) {
+        int h = hash(key);
+        Iterator<Integer> iterator = data[h].iterator();
+        while (iterator.hasNext()) {
+            Integer element = iterator.next();
+            if (element == key) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ T1/src/HashTable/easy/q705/q705.java
@@ -0,0 +1,11 @@
+package HashTable.easy.q705;
+
+/**
+ * @author Vikiang
+ * @URL https://leetcode.com/problems/design-hashset/
+ */
+public class q705 {
+    public static void main(String[] args) {
+
+    }
+}
